339773bdd6fbbdfb7bf4240fa12636d6def33074
m_shadkam <m.shadkam@isc.co.ir>
2024-05-27 14:02:03 +0330

    add mfe1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { loadRemoteModule } from '@angular-architects/native-federation';
+export const routes: Routes = [
+  {
+    path: 'flights',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+  },
+];
